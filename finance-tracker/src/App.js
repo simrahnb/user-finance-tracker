9a42cdb0ec89367dc60604be1af137124633166e
sim <simrahnajeeb@gmail.com>
2023-02-08 12:50:13 +0500

    Created the Home, Login & Signup page

--- finance-tracker/src/App.js
+++ finance-tracker/src/App.js
@@ -1,8 +1,26 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
+// pages & components
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Home /> 
+        </Route>
+        <Route exact path="/login">
+          <Login /> 
+        </Route>
+        <Route exact path="/signup">
+          <Signup /> 
+        </Route>
+      </Switch>
+      </BrowserRouter>
     </div>
   );
 }
