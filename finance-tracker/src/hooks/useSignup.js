5d8c60328f918cec38b2e3e8c32337ee17628705
sim <simrahnajeeb@gmail.com>
2023-02-08 15:02:15 +0500

    Added cleanup functions

--- finance-tracker/src/hooks/useSignup.js
+++ finance-tracker/src/hooks/useSignup.js
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { projectAuth } from "../firebase/config";
 import { useAuthContext } from "./useAuthContext";
 
 export const useSignup = () => {
+  const [isCancelled, setIsCancelled] = useState(false)
   const [error, setError] = useState(null);
   const [isPending, setIsPending] = useState(false);
   const { dispatch } = useAuthContext()
@@ -28,15 +29,26 @@ export const useSignup = () => {
       // dispatch login action
       dispatch({ type: 'LOGIN', payload: res.user })
 
+      // update state
+      if (!isCancelled) {
+        setIsPending(false)
+        setError(null)
+    }
       setIsPending(false);
       setError(null);
 
     } catch (err) {
+    if (!isCancelled) {
       console.log(err.message);
       setError(err.message);
       setIsPending(false);
     }
+    }
   };
+
+  useEffect(() => {
+    return () => setIsCancelled(true)
+}, [])
 
   return { error, isPending, signup };
 };
