ed949b6ca51a317625bdc2a223b7490012247e32
sim <simrahnajeeb@gmail.com>
2023-02-08 12:59:21 +0500

    Created the Navbar

--- finance-tracker/src/App.js
+++ finance-tracker/src/App.js
@@ -4,22 +4,24 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Switch>
-        <Route exact path="/">
-          <Home /> 
-        </Route>
-        <Route exact path="/login">
-          <Login /> 
-        </Route>
-        <Route exact path="/signup">
-          <Signup /> 
-        </Route>
-      </Switch>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/signup">
+            <Signup />
+          </Route>
+        </Switch>
       </BrowserRouter>
     </div>
   );
