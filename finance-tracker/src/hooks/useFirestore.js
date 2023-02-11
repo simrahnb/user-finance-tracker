d91824a1c49d2314597085a5e3504b3cf5d1936e
sim <simrahnajeeb@gmail.com>
2023-02-11 19:01:05 +0500

    ImplementingFirestore Timestamp

--- finance-tracker/src/hooks/useFirestore.js
+++ finance-tracker/src/hooks/useFirestore.js
@@ -1,5 +1,5 @@
 import { useReducer, useEffect, useState } from "react";
-import { projectFirestore } from "../firebase/config";
+import { projectFirestore, timestamp } from "../firebase/config";
 
 let intialState = {
   document: null,
@@ -10,6 +10,29 @@ let intialState = {
 
 const firestoreReducer = (state, action) => {
   switch (action.type) {
+    case "IS_PENDING":
+      return {
+        ...state,
+        isPending: true,
+        document: null,
+        success: false,
+        error: null,
+      };
+    case "ADDED_DOCUMENT":
+      return {
+        ...state,
+        isPending: false,
+        document: action.payload,
+        success: true,
+        error: null,
+      };
+    case "ERROR":
+      return {
+        isPending: false,
+        document: null,
+        success: false,
+        error: action.payload,
+      };
     default:
       return state;
   }
@@ -23,8 +46,28 @@ export const useFirestore = (collection) => {
   // collection ref
   const ref = projectFirestore.collection(collection);
 
+  // only dispatch if not cancelled
+  const dispatchIfNotCancelled = (action) => {
+    if (!isCancelled) {
+      dispatch(action);
+    }
+  };
+
   // add a document
-  const addDocument = (doc) => {};
+  const addDocument = async (doc) => {
+    dispatch({ type: "IS_PENDING" });
+
+    try {
+      const createdAt = timestamp.fromDate(new Date());
+      const addedDocument = await ref.add({ ...doc, createdAt });
+      dispatchIfNotCancelled({
+        type: "ADDED_DOCUMENT",
+        payload: addedDocument,
+      });
+    } catch (err) {
+      dispatchIfNotCancelled({ type: "ERROR", payload: err.message });
+    }
+  };
 
   // delete a document
   const deleteDocument = (id) => {};
